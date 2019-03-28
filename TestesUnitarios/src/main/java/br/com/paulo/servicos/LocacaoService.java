e0c99606f55a672a9ddd435bc8152e7cd7dccd91
pauloh04 <pauloh04@gmail.com>
2019-03-28 10:18:46 -0300

    Unit tests without tools

---
+++ TestesUnitarios/src/main/java/br/com/paulo/servicos/LocacaoService.java
@@ -0,0 +1,46 @@
+package br.com.paulo.servicos;
+
+import static br.com.paulo.utils.DataUtils.adicionarDias;
+
+import java.util.Date;
+
+import br.com.paulo.entidades.Filme;
+import br.com.paulo.entidades.Locacao;
+import br.com.paulo.entidades.Usuario;
+import br.com.paulo.utils.DataUtils;
+
+public class LocacaoService {
+	
+	public Locacao alugarFilme(Usuario usuario, Filme filme) {
+		Locacao locacao = new Locacao();
+		locacao.setFilme(filme);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		locacao.setValor(filme.getPrecoLocacao());
+
+		//Entrega no dia seguinte
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+		
+		//Salvando a locacao...	
+		//TODO adicionar método para salvar
+		
+		return locacao;
+	}
+	
+	public static void main(String[] args) {
+		//cenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 2, 5.0);
+		
+		//acao
+		Locacao locacao = service.alugarFilme(usuario, filme);
+		
+		//verificacao
+		System.out.println(locacao.getValor() == 5.0);
+		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+	}
+}
