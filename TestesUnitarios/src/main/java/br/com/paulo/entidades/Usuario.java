7fe563c332c53176bc8dbb9759495e28ecdf70e1
pauloh04 <pauloh04@gmail.com>
2019-03-28 11:01:05 -0300

    Assert types

--- TestesUnitarios/src/main/java/br/com/paulo/entidades/Usuario.java
+++ TestesUnitarios/src/main/java/br/com/paulo/entidades/Usuario.java
@@ -3,9 +3,10 @@ package br.com.paulo.entidades;
 public class Usuario {
 
 	private String nome;
-	
-	public Usuario() {}
-	
+
+	public Usuario() {
+	}
+
 	public Usuario(String nome) {
 		this.nome = nome;
 	}
@@ -17,4 +18,24 @@ public class Usuario {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return true;
+	}
+
 }
--- TestesUnitarios/src/test/java/br/com/paulo/servicos/LocacaoServiceTest.java
+++ TestesUnitarios/src/test/java/br/com/paulo/servicos/LocacaoServiceTest.java
@@ -1,6 +1,6 @@
 package br.com.paulo.servicos;
 
-import static br.com.paulo.utils.DataUtils.adicionarDias;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Date;
@@ -12,25 +12,7 @@ import br.com.paulo.entidades.Locacao;
 import br.com.paulo.entidades.Usuario;
 import br.com.paulo.utils.DataUtils;
 
-public class LocacaoService {
-	
-	public Locacao alugarFilme(Usuario usuario, Filme filme) {
-		Locacao locacao = new Locacao();
-		locacao.setFilme(filme);
-		locacao.setUsuario(usuario);
-		locacao.setDataLocacao(new Date());
-		locacao.setValor(filme.getPrecoLocacao());
-
-		//Entrega no dia seguinte
-		Date dataEntrega = new Date();
-		dataEntrega = adicionarDias(dataEntrega, 1);
-		locacao.setDataRetorno(dataEntrega);
-		
-		//Salvando a locacao...	
-		//TODO adicionar método para salvar
-		
-		return locacao;
-	}
+public class LocacaoServiceTest {
 	
 	@Test
 	public void teste() {
@@ -43,7 +25,7 @@ public class LocacaoService {
 		Locacao locacao = service.alugarFilme(usuario, filme);
 		
 		//verificacao
-		assertTrue(5.0 == locacao.getValor());
+		assertEquals(5.0, locacao.getValor(), 0.01);
 		assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
 		assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 	}
